a5bea2b85c76ac7c2fcdc6d7c1ffe65ede8b62c2
adhil muhammed <adhil.muhammed@codinghands.in>
2023-07-14 02:03:11 +0530

    code cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div className=" text-center  flex flex-col justify-center items-center min-h-screen ">
-      <h1 className="text-center flex justify-center text-4xl text-white font-bold">
+      <h1 className=" text-4xl text-white font-bold">
         This is next js tutorial project
       </h1>
       <div>
